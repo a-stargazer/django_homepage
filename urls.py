f90b7eb4343a55965b5e1ff8bc3e681ea6a78a23
Alice <aliceyang013@gmail.com>
2020-04-18 23:01:05 -0700

    added url patterns to url.py

--- urls.py
+++ urls.py
@@ -1,22 +1,16 @@
-# This file has the "routing" -- it associates "view functions" (found in
-# views.py) with different paths.
-
 from django.urls import path
 
 import views
 
-# In this example, we've separated out the views.py into a new file
 urlpatterns = [
-    path('', views.index),
-    path('about-me', views.about_me),
-    path('github-api-example', views.github_api_example),
-    path("newstuff", views.newstuff)
+    path('', views.homepage),
+    path('projects', views.projects),
+    path('about-me', views.about),
+    path('blog', views.blog),
+    path('repos', views.github_api),
 ]
 
 # Boilerplate to include static files.
-# Static files include CSS and images -- basically anything that isn't HTML or
-# Python code -- stuff we just want to "serve up" for the browser to download
-# and utilize and won't be changed by Python code (hence the term "static").
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns += static('static/', document_root=settings.STATIC_ROOT)
